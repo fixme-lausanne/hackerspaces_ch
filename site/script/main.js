87d0975156cbe76d7267b7f748f5e13f1fee616c
gcmalloc <gcmalloc@gmail.com>
2013-05-14 22:15:13 +0200

    adding the openlayer map instead of the google one

--- site/script/main.js
+++ site/script/main.js
@@ -1,14 +1,28 @@
 function loadmap(){
-    map = new OpenLayers.Map("demoMap");
-    map.addLayer(new OpenLayers.Layer.OSM());
+    map = new OpenLayers.Map("demoMap")
 
+    //var source = Proj4js.Proj('EPSG:4236'); 
+    map.addLayer(new OpenLayers.Layer.OSM());
+    $.getJSON('http://127.0.0.1:8080/list', function(data){loadMarker(map, data)})
+    map.zoomToMaxExtent();
+}
+
+function loadMarker(map, data) {
     var markers = new OpenLayers.Layer.Markers("Markers");
     map.addLayer(markers);
-    var size = new OpenLayers.Size(21,25);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+    $.each(data, function(key, value) {
+        var size = new OpenLayers.Size(21,25);
+        var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+    //markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0, 0),icon));
+        var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+        var position = value.coordinate;
+        console.log(position);
+        var LonLat = new OpenLayers.LonLat( position[1],position[0])
+       .transform(
+           new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+           map.getProjectionObject()); // to Spherical Mercator Projection
 
-    markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon));
-
-    map.zoomToMaxExtent();
+        markers.addMarker(new OpenLayers.Marker(LonLat,icon));
+        });
+    map.zoomToExtent(markers.getDataExtent())
 }
