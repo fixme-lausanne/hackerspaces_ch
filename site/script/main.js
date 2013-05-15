71e682c49714202768d9afbd6c874e70f0ed28e1
gcmalloc <gcmalloc@gmail.com>
2013-05-15 19:03:41 +0200

    a bit of cleaning to the code

--- site/script/main.js
+++ site/script/main.js
@@ -1,41 +1,37 @@
 function loadmap(){
     map = new OpenLayers.Map("demoMap")
 
-    //var source = Proj4js.Proj('EPSG:4236'); 
     map.addLayer(new OpenLayers.Layer.OSM());
     $.getJSON('list', function(data){loadMarker(map, data)})
     map.zoomToMaxExtent();
 }
 
 function loadMarker(map, data) {
-    var markers = new OpenLayers.Layer.Markers("Markers");
-    map.addLayer(markers);
+    var markersLayer = new OpenLayers.Layer.Markers("Markers");
+    map.addLayer(markersLayer);
     $.each(data, function(key, value) {
         var size = new OpenLayers.Size(21,25);
         var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
         var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
         var position = value.coordinate;
-        console.log(position);
-        var lonLat = new OpenLayers.LonLat( position[1],position[0])
+        var lonLat = new OpenLayers.LonLat(position[1],position[0])
        .transform(
            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
            map.getProjectionObject()); // to Spherical Mercator Projection
-        marker = new OpenLayers.Marker(lonLat, icon)
-        markers.addMarker(marker);
-        var popup = new OpenLayers.Popup(key,
+        var marker = new OpenLayers.Marker(lonLat, icon)
+        markersLayer.addMarker(marker);
+        var popup = new OpenLayers.Popup.AnchoredBubble(key,
                                 lonLat,
                                 new OpenLayers.Size(200,200),
                                 JSON.stringify(value, null, '\t'), 
+                                null,
                                 true);
-        console.log("added popup");
         marker.events.register("click", marker, function (e) {
-            map.addPopup(popup,true);
+            map.addPopup(popup);
             popup.show();
         });
     });
-    map.zoomToExtent(markers.getDataExtent())
+    map.zoomToExtent(markersLayer.getDataExtent())
 
 }
 
-function getClosest() {
- }
