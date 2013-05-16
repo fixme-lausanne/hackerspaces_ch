9eda02e2f5b64dd16a26df6bd6c052562f073090
gcmalloc <gcmalloc@gmail.com>
2013-05-16 04:27:49 +0200

    removing the bottle application as it is not used anymore

--- hackerspace_scraper/api.py
+++ hackerspace_scraper/api.py
@@ -16,66 +16,49 @@ non_numeric = re.compile('[^,\d.]+')
 BASE_URL = "http://hackerspaces.org"
 base_url = partial(urljoin, BASE_URL)
 SWISS_HS = "/wiki/switzerland"
-HS_URL = "http://hackerspaces.org/w/index.php?title={0}&action=edit"
+HS_URL = base_url("w/index.php?title={0}&action=edit")
 LOCATION_STRING = "coordinate"
 SPACE_API = "http://openspace.slopjong.de/directory.json"
 
-app = bottle.Bottle("hackerspace.ch")
-
-CachedValue = namedtuple('CachedValue', ['time', 'value'])
-
-class Cache(object):
-    def __init__(self):
-        self.cache = {}
-
-    def cached(self, timeout):
-        def decorator(f):
-            def get_value(*args, **kwargs):
-                try:
-                    value = self.cache[f]
-                    if time.time() > (value.time + timeout):
-                        #too old 
-                        raise KeyError
-                except KeyError:
-                    value = CachedValue(time.time(), f(*args, **kwargs))
-                    self.cache[f] = value
-                return value.value
-
-            #the new function wrapped
-            @functools.wraps(f)
-            def decorated_function(*args, **kwargs):
-                return get_value(*args, **kwargs)
-            return decorated_function
-        return decorator
-
-cache = Cache()
-
-@cache.cached(timeout=60*60)
-def get_hackerspaces():
-    resp = requests.get(base_url(SWISS_HS))
+def get_etree(url):
+    """Return a etree from an url using request
+    """
+    resp = requests.get(url)
     tree = etree.parse(io.StringIO(resp.text))
+    return tree
+
+def get_hackerspaces():
+    tree = get_etree(base_url(SWISS_HS))
     links = tree.xpath('//*[@id="mw-content-text"]/table[2]//td//text()')
     hs_names = links[::2]
-    coordinates = links[1::2]
+
     hackerspaces = {}
     for name in hs_names:
         hackerspaces[name] = get_hackerspace(name)
-        hackerspaces[name]['space_url'] = get_space_api_url(name)
+
+        #space api url
+        space_url = get_space_api_url(name)
+        if space_url:
+            hackerspaces[name]['space_url'] = space_url
     return hackerspaces
 
 def get_space_api_url(name):
     req = requests.get(SPACE_API)
     j = json.loads(req.text)
-    return j.get(name, '')
+    return j.get(name)
     
 def get_hackerspace(name):
     url = HS_URL.format(name)
-    resp = requests.get(url)
-    tree = etree.parse(io.StringIO(resp.text))
+    tree = get_etree(url)
+
     text_box = tree.xpath('//*[@id="wpTextbox1"]//text()')
     text = "".join(text_box)
+
+    #select the first string between {{ and }}
+    #this use a simple descent parser
     count = 0
     result = ""
+
     for char in text:
         if char == '{':
             count += 1
@@ -85,30 +68,30 @@ def get_hackerspace(name):
                 break
         elif count == 2:
             result += char
+
+    #split over | for key value pair
+    #and split over = for key value
     ret = {}
     for kv in result.split("|"):
         try:
-            k, v = map(lambda a: a.replace('\n', ' ').strip(), kv.split("="))
-            if k == LOCATION_STRING:
-                v = clean_location(v)
-            ret[k] = v
+            key, value = [el.replace('\n', ' ').strip() for el in kv.split("=")]
+            if key == LOCATION_STRING:
+                value = clean_location(value)
+            ret[key] = value
         except ValueError:
             pass
     return ret
 
 def clean_location(location_string):
-    return non_numeric.sub('', location_string).split(",")
-
-
-
-@app.route('/list')
-def list_hackerspaces():
-    return get_hackerspaces()
-
+    """This method will remove all non numeric or commas or dot from a string, then
+    split it on the comma and select only the 2 first element. This has the goal to 
+    clean any string that is malformed
+    """
+    return non_numeric.sub('', location_string).split(",")[:2]
 
 if __name__ == '__main__':
     file_path = os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), "site/list")
     with open(file_path, 'w') as f:
-        hs = list_hackerspaces()
+        hs = get_hackerspaces()
         print(hs)
         json.dump(hs, f)
