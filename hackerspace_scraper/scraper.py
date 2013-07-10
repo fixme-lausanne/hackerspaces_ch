9e59a6294170553c08ec22b98a9244b5c8cc9143
gcmalloc <gcmalloc@gmail.com>
2013-07-10 20:23:06 +0200

    refactoring the scraper

--- hackerspace_scraper/scraper.py
+++ hackerspace_scraper/scraper.py
@@ -1,138 +1,147 @@
 from lxml import etree
+from urlparse import urljoin
+import io
 import json
 import os
-import io
-import requests
-from urlparse import urljoin
-from functools import partial
-import bottle
-import functools
-from collections import namedtuple
-import time
 import re
+import requests
+import sys
 
-non_numeric = re.compile('[^,\d.]+')
 
-BASE_URL = "http://hackerspaces.org"
-def absolute_url(path):
-    return urljoin(BASE_URL, path)
+file_path = os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), "site/list")
 
-SWISS_HS = "/wiki/switzerland"
-HS_URL = absolute_url("w/index.php?title={0}&action=edit")
-LOCATION_KEY = "coordinate"
-LOGO_KEY = "logo"
-SPACE_API = "http://spaceapi.net/directory.json"
+class Hackerspaces(object):
+    LOCATION_KEY = "coordinate"
+        LOGO_KEY = "logo"
+    BASE_URL = "http://hackerspaces.org"
 
-def url_for_file(filename):
-    url = "http://hackerspaces.org/wiki/File:{0}".format(filename)
-    tree = get_etree(url)
-    url = tree.xpath('//a[@class="internal"]/@href')
-    if len(url) == 1:
-        return url[0]
+    @staticmethod
+    def absolute_url(path):
+        return urljoin(Hackerspaces.BASE_URL, path)
 
-def get_etree(url, browser_dump=False):
-    """Return a etree from an url using request
-    """
-    resp = requests.get(url)
-    if browser_dump:
-        with open('dump', 'w') as f:
-            f.write(resp.content)
-        import webbrowser
-        webbrowser.open('dump')
-        exit(0)
+    @staticmethod
+    def wiki_page(title):
+        return urljoin(Hackerspaces.absolute_url('wiki/'), title)
 
-    tree = etree.parse(io.StringIO(resp.text))
-    return tree
-
-def get_hackerspaces():
-    tree = get_etree(absolute_url(SWISS_HS))
-    hs_names = tree.xpath('//*[@id="mw-content-text"]/table[1]/tr[2]/td[1]/ul[1]/li//text()')
-    links = tree.xpath('//*[@id="mw-content-text"]/table[1]/tr[2]/td[1]/ul[1]/li/a/@href')
-
-    space_api = json.loads(requests.get(SPACE_API).text)
-    hackerspaces = {}
-    for name in hs_names:
-        hackerspaces[name] = get_hackerspace(name)
-
-        #space api url
-        space_url = get_space_api_url(space_api, hackerspaces, name)
-        if space_url:
-            hackerspaces[name]['space_url'] = space_url
-    return hackerspaces
-
-def get_space_api_url(space_api, hackerspaces, name):
-    url = space_api.get(name)
-    if url:
-        print '%s has %s' % (name, url)
+    @staticmethod
+    def edit_page(title):
+        url = Hackerspaces.absolute_url("w/index.php?title={0}&action=edit".format(title))
         return url
-    else:
-        if 'site' in hackerspaces[name]:
-            for i in space_api.values():
-                if re.match('%s.*'%hackerspaces[name]['site'], i):
-                    print '%s has %s' % (name, i)
-                    return i
+
+    @staticmethod
+    def url_for_file(filename):
+        """Format the url 
+        """
+        url = Hackerspaces.wiki_page('File') + ":{0}".format(filename)
+        tree = Hackerspaces.get_etree(url)
+        url = tree.xpath('//a[@class="internal"]/@href')
+        if len(url) == 1:
+            return url[0]
+
+    #regex matching all the non numeric character including 
+    NON_NUMERIC = re.compile('[^,\d.]+')
+
+    #url used to fetch information about the space api
+    SPACE_API = "http://spaceapi.net/directory.json"
+
+    def __init__(self, country):
+        """docstring for """
+        super(Hackerspaces, self).__init__()
+        self.country = country
+        #load the space api directory
+
+
+
+    @staticmethod
+    def get_etree(url, browser_dump=False):
+        """Return a etree from an url using request
+        """
+        print url
+        resp = requests.get(url)
+        if browser_dump:
+            with open('dump', 'w') as f:
+                f.write(resp.content)
+            import webbrowser
+            webbrowser.open('dump')
+            exit(0)
+
+        tree = etree.parse(io.StringIO(resp.text))
+        return tree
+
+    def get_hackerspaces(self):
+        tree = Hackerspaces.get_etree(Hackerspaces.wiki_page(self.country))
+        hs_names = tree.xpath('//*[@id="mw-content-text"]/table[1]/tr[2]/td[1]/ul[1]/li//text()')
+        #links = tree.xpath('//*[@id="mw-content-text"]/table[1]/tr[2]/td[1]/ul[1]/li/a/@href')
+
+        hackerspaces = {}
+        for name in hs_names:
+            print name
+            hackerspaces[name] = self.get_hackerspace(name)
+
+            #space api url
+            space_url = self.get_space_api_url(name)
+            if space_url:
+                hackerspaces[name]['space_url'] = space_url
+        return hackerspaces
+
+    def get_space_api_url(self, name):
+        req = requests.get(Hackerspaces.SPACE_API)
+        j = json.loads(req.text)
+        return j.get(name)
+
+    def get_hackerspace(self, name):
+        url = Hackerspaces.edit_page(name)
+        tree = self.get_etree(url)
+
+        text_box = tree.xpath('//*[@id="wpTextbox1"]//text()')
+        text = "".join(text_box)
+
+        #select the first string between {{ and }}
+        #this use a simple descent parser
+        count = 0
+        result = ""
+
+        for char in text:
+            if char == '{':
+                count += 1
+            elif char == '}':
+                count -= 1
+                if count == 0 and result:
+                    break
+            elif count == 2:
+                result += char
+
+        #split over | for key value pair
+        #and split over = for key value
+        ret = {}
+        for kv in result.split("|"):
             try:
-                url = '%s/status.json' % hackerspaces[name]['site']
-                if not url.startswith('http'):
-                    url = 'http://%s' % url
-                req = requests.get(url)
-                if req.status_code==200 and 'json' in req.headers['content-type']:
-                    print '%s has %s' % (name, url)
-                    return url
-            except Exception, e:
-                print e
-        print '%s has no spaceapi' % name
-
-def get_hackerspace(name):
-    url = HS_URL.format(name.encode('utf-8'))
-    tree = get_etree(url)
-
-    text_box = tree.xpath('//*[@id="wpTextbox1"]//text()')
-    text = "".join(text_box)
-
-    #select the first string between {{ and }}
-    #this use a simple descent parser
-    count = 0
-    result = ""
-
-    for char in text:
-        if char == '{':
-            count += 1
-        elif char == '}':
-            count -= 1
-            if count == 0 and result:
-                break
-        elif count == 2:
-            result += char
-
-    #split over | for key value pair
-    #and split over = for key value
-    ret = {}
-    for kv in result.split("|"):
-        try:
-            key, value = [el.replace('\n', ' ').strip() for el in kv.split("=")]
-            if key == LOCATION_KEY:
-                value = clean_location(value)
-            elif key == LOGO_KEY:
+                key, value = [el.replace('\n', ' ').strip() for el in kv.split("=")]
+                if key == Hackerspaces.LOCATION_KEY:
+                    value = Hackerspaces.clean_location(value)
+                elif key == Hackerspaces.LOGO_KEY:
+                    if value:
+                        value = Hackerspaces.absolute_url(Hackerspaces.url_for_file(value))
                 if value:
-                    value = absolute_url(url_for_file(value))
-            if value:
-                ret[key] = value
-        except ValueError:
-            pass
-    return ret
+                    ret[key] = value
+            except ValueError:
+                pass
+        return ret
 
-
-def clean_location(location_string):
-    """This method will remove all non numeric or commas or dot from a string, then
-    split it on the comma and select only the 2 first element. This has the goal to
-    clean any string that is malformed
-    """
-    return non_numeric.sub('', location_string).split(",")[:2]
+    @staticmethod
+    def clean_location(location_string):
+        """This method will remove all non numeric or commas or dot from a string, then
+        split it on the comma and select only the 2 first element. This has the goal to
+        clean any string that is malformed
+        """
+        return hackerspaces.NON_NUMERIC.sub('', location_string).split(",")[:2]
 
 if __name__ == '__main__':
-    file_path = os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), "site/list")
+    if len(sys.argv) == 3:
+        hackerspaces = Hackerspaces(sys.argv[1])
+    else:
+        hackerspaces = Hackerspaces('switzerland')
     with open(file_path, 'w') as f:
-        hs = get_hackerspaces()
-        #print(hs)
+        hs = hackerspaces.get_hackerspaces()
+        print(hs)
         json.dump(hs, f)
